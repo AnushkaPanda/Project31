4b36b5072fc3883f9044fe8f0874255c7702bac4
Anushka Panda <73485171+AnushkaPanda@users.noreply.github.com>
2020-12-10 21:34:54 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,31 @@
+class Particle {
+    constructor(x, y,r) {
+      var options = {
+          isStatic:false,
+          restitution:0,
+          friction:1,
+          density:1.2
+      }
+      this.x=x;
+		this.y=y;
+		this.r=r;
+		
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+
+			var particlepos=this.body.position;		
+
+			push()
+			translate(particlepos.x, particlepos.y);
+			ellipse(0,0,this.r)
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(255)
+			pop()
+
+	}
+}
---
+++ plinko.js
@@ -0,0 +1,29 @@
+class Plinko {
+    constructor(x, y,r) {
+      var options = {
+          isStatic:true
+          
+      }
+      this.x=x;
+		this.y=y;
+		this.r=r;
+		
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+
+			var plinkopos=this.body.position;		
+
+			push()
+			translate(plinkopos.x, plinkopos.y);
+			ellipse(0,0,this.r)
+			rectMode(CENTER)
+			strokeWeight(3);
+			fill(255)
+			pop()
+
+	}
+}
